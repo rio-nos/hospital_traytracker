8c80d4e88f3f53cb52c68e20ff99f5ccf4191bdd
rio-nos <nlibe.or@gmail.com>
2021-04-07 20:32:55 -0700

    Finished initial/initial columns. Added "Del Number" at the very top to specifiy which job you are. Fixed data validation. Fixed "Delivery Time Summary" columns. Added finishing touches to colors. 18 sheets.

--- tray_script/main.py
+++ tray_script/main.py
@@ -4,7 +4,7 @@ import os
 from datetime import timedelta, date
 
 
-tray_tracker = './TrayTrackerColors.xlsm'
+tray_tracker = './TrayTracker.xlsm'
 
 years = {x: str(x) for x in range(2021, 2021+1)}
 months = {0: 'January', 1: 'February', 2: 'March', 3: 'April', 4: 'May', 5: 'June',
@@ -31,23 +31,25 @@ def yield_dates(start, end):
 
 
 def main():
+    print(f'Opening {tray_tracker}')
     # make_directories()
     xw.App(visible=False)
     excel_tray_tracker = xw.Book(tray_tracker)
     app = xw.apps.active
-    start = date(2021, 3, 1)
-    end = date(2021, 4, 1)
+    start = date(2021, 4, 9)
+    end = date(2021, 5, 1)
     for current_date in yield_dates(start, end):
         s = current_date.split()
         print(s)
-        for i in range(19, -1, -1):
+        for i in range(20):
             current_sheet = excel_tray_tracker.sheets[i]
+            current_sheet.range('E8').value = 'Initials'
             current_sheet.range('D2').value = s[0]
             current_sheet.range('D3').value = "'" + s[1] + ", " + s[3] + ", " + s[4]
         new_directory = './' + s[-1] + '/' + s[2] + '-' + s[1] + '/'
         new_filename = s[2]+'-'+s[3]+'-'+s[4]+'.xlsm'
         excel_tray_tracker.save(new_directory + new_filename)
-    sleep(3)
+    sleep(1.5)
     app.quit()
     app.kill()
 
